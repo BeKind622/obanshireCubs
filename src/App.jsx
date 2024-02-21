4ac6549811db46c88d16e5995d4c5964475b695d
BeKind622 <raulgalportfolio@gmail.com>
2024-02-21 14:13:11 +0000

    nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
     
     <Router>
       <div>
-        <nav>
+        {/* <nav>
           <ul>
             <li>
               <Link to="/">Home </Link>
@@ -23,8 +23,41 @@ const App = () => {
               <Link to="/register">Register </Link>
             </li>
           </ul>
-        </nav>
-
+        </nav> */}
+ <div class="px-7 bg-white shadow-lg rounded-2xl mb-5">
+            <div class="flex">
+                <div class="flex-auto hover:w-full group">
+                <Link to="/" class="flex items-center justify-center text-center mx-auto px-4 py-2 group-hover:w-full text-indigo-500"> 
+                    
+                        <span class="block px-1 py-1 group-hover:bg-indigo-100 rounded-full group-hover:flex-grow">
+                            <i class="far fa-home text-2xl pt-1"></i><span class="hidden group-hover:inline-block ml-3 align-bottom pb-1">Home</span>
+                        </span>
+                        </Link>
+                </div>
+                <div class="flex-auto hover:w-full group">
+                <Link to="/login" class="flex items-center justify-center text-center mx-auto px-4 py-2 group-hover:w-full text-indigo-500"> 
+                        <span class="block px-1 py-1 group-hover:bg-indigo-100 rounded-full group-hover:flex-grow">
+                            <i class="far fa-compass text-2xl pt-1"></i><span class="hidden group-hover:inline-block ml-3 align-bottom pb-1">Login</span>
+                        </span>
+                    </Link>
+                </div>
+                <div class="flex-auto hover:w-full group">
+                <Link to="/register" class="flex items-center justify-center text-center mx-auto px-4 py-2 group-hover:w-full text-indigo-500"> 
+                        <span class="block px-1 py-1 group-hover:bg-indigo-100 rounded-full group-hover:flex-grow">
+                            <i class="far fa-search text-2xl pt-1"></i><span class="hidden group-hover:inline-block ml-3 align-bottom pb-1">Search</span>
+                        </span>
+                    </Link>
+                </div>
+                <div class="flex-auto hover:w-full group">
+                    <a href="#" class="flex items-center justify-center text-center mx-auto px-4 py-2 group-hover:w-full text-indigo-500">
+                        <span class="block px-1 py-1 group-hover:bg-indigo-100 rounded-full group-hover:flex-grow">
+                            <i class="far fa-cog text-2xl pt-1"></i><span class="hidden group-hover:inline-block ml-3 align-bottom pb-1">Settings</span>
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <hr />
         <hr />
 
         <Routes>
