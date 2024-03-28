6a6e126cba3ed4009d25f363ac5e596c10bb593a
BeKind622 <116099798+BeKind622@users.noreply.github.com>
2024-03-28 23:15:36 +0000

    little to no changes made

--- src/pages/shared/Home.jsx
+++ src/pages/shared/Home.jsx
@@ -4,92 +4,92 @@ const Home = () => {
   return (
     <>
       
-<div class="w-screen h-screen overflow-hidden relative before:block before:absolute before:bg-black before:h-full before:w-full before:top-0 before:left-0 before:z-10 before:opacity-30">
-  <img src="https://picsum.photos/seed/picsum/1900/850" class="absolute top-0 left-0 min-h-full ob" alt=""/>
-  <div class="relative z-20 max-w-screen-lg mx-auto grid grid-cols-12 h-full items-center">
-    <div class="col-span-6">
-      <span class="uppercase text-white text-xs font-bold mb-2 block">WE ARE EXPERTS</span>
-      <h1 class="text-white font-extrabold text-5xl mb-8">Finpoint provides Financial Consulting in different ways</h1>
-      <p class="text-stone-100 text-base">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+<div className="w-screen h-screen overflow-hidden relative before:block before:absolute before:bg-black before:h-full before:w-full before:top-0 before:left-0 before:z-10 before:opacity-30">
+  <img src="https://images.unsplash.com/photo-1586773860418-d37222d8fce3?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="absolute top-0 left-0 min-h-full ob" alt=""/>
+  <div className="relative z-20 max-w-screen-lg mx-auto grid grid-cols-12 h-full items-center">
+    <div className="col-span-6">
+      <span className="uppercase text-white text-xs font-bold mb-2 block">WE ARE EXPERTS</span>
+      <h1 className="text-white font-extrabold text-5xl mb-8">We are the hospital that heals people</h1>
+      <p className="text-stone-100 text-base">
+        Any ailments, sicknesses or illnesses you have, we'll cure them right up.
       </p>
-      <button class="mt-8 text-white uppercase py-4 text-base font-light px-10 border border-white hover:bg-white hover:bg-opacity-10">Get started</button>
+      <button className="mt-8 text-white uppercase py-4 text-base font-light px-10 border border-white hover:bg-white hover:bg-opacity-10">Get started</button>
     </div>
   </div>
 </div>
-<div class="bg-[#f7d0b6] py-20">
-  <div class="max-w-screen-lg mx-auto flex justify-between items-center">
-    <div class="max-w-xl">
-      <h2 class="font-black text-sky-950 text-3xl mb-4">As the leading experts in this field, we're in over 90 countries</h2>
-      <p class="text-base text-sky-950">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+{/* <div className="bg-[#f7d0b6] py-20">
+  <div className="max-w-screen-lg mx-auto flex justify-between items-center">
+    <div className="max-w-xl">
+      <h2 className="font-black text-sky-950 text-3xl mb-4">As the leading experts in this field, we're in over 90 countries</h2>
+      <p className="text-base text-sky-950">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
     </div>
-    <button class="text-white  uppercase py-3 text-base px-10 border border-sky-950 hover:bg-sky-950 hover:bg-opacity-10">Get started</button>
+    <button className="text-white  uppercase py-3 text-base px-10 border border-sky-950 hover:bg-sky-950 hover:bg-opacity-10">Get started</button>
   </div>
-</div>
-<div class="py-12 relative overflow-hidden bg-white">
-  <div class="grid grid-cols-2 max-w-screen-lg mx-auto">
-    <div class="w-full flex flex-col items-end pr-16">
-      <h2 class="text-[#64618C] font-bold text-2xl max-w-xs text-right mb-12 mt-10">Whether you need Assistance</h2>
-      <div class="h-full mt-auto overflow-hidden relative">
-        <img src="https://picsum.photos/800/600" class="h-full w-full object-contain" alt=""/>
+</div> */}
+{/* <div className="py-12 relative overflow-hidden bg-white">
+  <div className="grid grid-cols-2 max-w-screen-lg mx-auto">
+    <div className="w-full flex flex-col items-end pr-16">
+      <h2 className="text-[#64618C] font-bold text-2xl max-w-xs text-right mb-12 mt-10">Whether you need Assistance</h2>
+      <div className="h-full mt-auto overflow-hidden relative">
+        <img src="https://picsum.photos/800/600" className="h-full w-full object-contain" alt=""/>
       </div>
     </div>
 
-    <div class="py-20 bg-slate-100 relative before:absolute before:h-full before:w-screen before:bg-sky-950 before:top-0 before:left-0">
-      <div class="relative z-20 pl-12">
-        <h2 class="text-[#f7d0b6] font-black text-5xl leading-snug mb-10">Finpoint is here <br/>to help you</h2>
-        <p class="text-white text-sm">
+    <div className="py-20 bg-slate-100 relative before:absolute before:h-full before:w-screen before:bg-sky-950 before:top-0 before:left-0">
+      <div className="relative z-20 pl-12">
+        <h2 className="text-[#f7d0b6] font-black text-5xl leading-snug mb-10">Finpoint is here <br/>to help you</h2>
+        <p className="text-white text-sm">
           Purus in massa tempor nec. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Faucibus ornare suspendisse sed nisi lacus sed viverra. Diam in arcu cursus euismod quis viverra nibh cras pulvinar.
         </p>
-        <button class="mt-8 text-white uppercase py-3 text-sm px-10 border border-white hover:bg-white hover:bg-opacity-10">Talk with expert</button>
+        <button className="mt-8 text-white uppercase py-3 text-sm px-10 border border-white hover:bg-white hover:bg-opacity-10">Talk with expert</button>
       </div>
     </div>
   </div>
 </div>
 
-<div class="py-4 relative overflow-hidden bg-white">
-  <div class="grid grid-cols-2 max-w-screen-lg mx-auto">
+<div className="py-4 relative overflow-hidden bg-white">
+  <div className="grid grid-cols-2 max-w-screen-lg mx-auto">
     
 
-    <div class="py-20 bg-slate-100 relative before:absolute before:h-full before:w-screen before:bg-[#f7d0b6] before:top-0 before:right-0">
-      <div class="relative z-20 pl-12">
-        <h2 class="text-sky-950 font-black text-5xl leading-snug mb-10">Finpoint is here <br/>to help you</h2>
-        <p class="text-sky-950 text-sm">
+    <div className="py-20 bg-slate-100 relative before:absolute before:h-full before:w-screen before:bg-[#f7d0b6] before:top-0 before:right-0">
+      <div className="relative z-20 pl-12">
+        <h2 className="text-sky-950 font-black text-5xl leading-snug mb-10">Finpoint is here <br/>to help you</h2>
+        <p className="text-sky-950 text-sm">
           Purus in massa tempor nec. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Faucibus ornare suspendisse sed nisi lacus sed viverra. Diam in arcu cursus euismod quis viverra nibh cras pulvinar.
         </p>
-        <button class="mt-8 text-sky-950 uppercase py-3 text-sm px-10 border border-sky-950 hover:bg-white hover:bg-opacity-10">Talk with expert</button>
+        <button className="mt-8 text-sky-950 uppercase py-3 text-sm px-10 border border-sky-950 hover:bg-white hover:bg-opacity-10">Talk with expert</button>
       </div>
     </div>
-    <div class="w-full flex flex-col pl-16">
-      <h2 class="text-[#64618C] font-bold text-2xl max-w-xs text-left mb-12 mt-10">Whether you need Assistance</h2>
-      <div class="h-full mt-auto overflow-hidden relative">
-        <img src="https://picsum.photos/800/600" class="h-full w-full object-contain" alt=""/>
+    <div className="w-full flex flex-col pl-16">
+      <h2 className="text-[#64618C] font-bold text-2xl max-w-xs text-left mb-12 mt-10">Whether you need Assistance</h2>
+      <div className="h-full mt-auto overflow-hidden relative">
+        <img src="https://picsum.photos/800/600" className="h-full w-full object-contain" alt=""/>
       </div>
     </div>
 
   </div>
 </div>
 
-<div class="py-12 relative overflow-hidden bg-white">
-  <div class="grid grid-cols-2 max-w-screen-lg mx-auto">
-    <div class="w-full flex flex-col items-end pr-16">
-      <h2 class="text-[#64618C] font-bold text-2xl max-w-xs text-right mb-12 mt-10">Whether you need Assistance</h2>
-      <div class="h-full mt-auto overflow-hidden relative">
-        <img src="https://picsum.photos/800/600" class="h-full w-full object-contain" alt=""/>
+<div className="py-12 relative overflow-hidden bg-white">
+  <div className="grid grid-cols-2 max-w-screen-lg mx-auto">
+    <div className="w-full flex flex-col items-end pr-16">
+      <h2 className="text-[#64618C] font-bold text-2xl max-w-xs text-right mb-12 mt-10">Whether you need Assistance</h2>
+      <div className="h-full mt-auto overflow-hidden relative">
+        <img src="https://picsum.photos/800/600" className="h-full w-full object-contain" alt=""/>
       </div>
     </div>
 
-    <div class="py-20 bg-slate-100 relative before:absolute before:h-full before:w-screen before:bg-sky-950 before:top-0 before:left-0">
-      <div class="relative z-20 pl-12">
-        <h2 class="text-[#f7d0b6] font-black text-5xl leading-snug mb-10">Finpoint is here <br/>to help you</h2>
-        <p class="text-white text-sm">
+    <div className="py-20 bg-slate-100 relative before:absolute before:h-full before:w-screen before:bg-sky-950 before:top-0 before:left-0">
+      <div className="relative z-20 pl-12">
+        <h2 className="text-[#f7d0b6] font-black text-5xl leading-snug mb-10">Finpoint is here <br/>to help you</h2>
+        <p className="text-white text-sm">
           Purus in massa tempor nec. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Faucibus ornare suspendisse sed nisi lacus sed viverra. Diam in arcu cursus euismod quis viverra nibh cras pulvinar.
         </p>
-        <button class="mt-8 text-white uppercase py-3 text-sm px-10 border border-white hover:bg-white hover:bg-opacity-10">Talk with expert</button>
+        <button className="mt-8 text-white uppercase py-3 text-sm px-10 border border-white hover:bg-white hover:bg-opacity-10">Talk with expert</button>
       </div>
     </div>
   </div>
-</div>
+</div> */}
     </>
   );
 };
