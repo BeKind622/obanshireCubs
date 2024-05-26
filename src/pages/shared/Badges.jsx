cac1f22a11cac36b3875e5d61134f1369e2cafe8
BeKind622 <116099798+BeKind622@users.noreply.github.com>
2024-05-26 10:53:07 +0100

    cosmetic changes

--- src/pages/shared/Badges.jsx
+++ src/pages/shared/Badges.jsx
@@ -70,10 +70,37 @@ const Badges = () => {
   };
 
   return (
-    <div>
+    <div className='bg-white'>
       
+      <div className="hero py-40 bg-slate-200 relative before:block before:absolute before:bg-black before:h-full before:w-full before:top-0 before:z-10 before:opacity-70">
+  <img src="https://images.unsplash.com/photo-1541797873665-6d4cc148885f?q=80&w=2031&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="absolute top-0 left-0 h-full w-full object-cover" />
+  <div className="relative z-20 mx-auto max-w-screen-xl flex gap-20 justify-center">
+    <div className="w-2/3">
+      <span className="text-orange-600 uppercase font-medium mb-4 block">Obanshire cub scouts</span>
+      <h1 className="text-7xl text-white font-extrabold">Badges</h1>
+      {/* <h2>{user.userType}</h2> */}
+      <div className="flex items-center gap-3 text-white my-6 text-4xl">
+      
+      </div>
+      <p className="text-slate-200">
+        Taciti quasi, sagittis excepteur hymenaeos, id temporibus hic proident ullam, eaque donec delectus tempor consectetur nunc, purus congue? Rem volutpat sodales! Mollit. Minus exercitationem wisi. Optio ullamco nullam purus volutpat optio ullamco nullam purus volutpat.
+      </p>
+      <div className="flex gap-8 mt-10">
+        {/* <button className="text-white h-12 bg-orange-700 w-44">
+          Read More
+        </button>
+        <button className="text-white h-12 bg-blue-500 w-44">
+          See all offert
+        </button> */}
+      </div>
+    </div>
+    <div className="w-1/3">
+      
+    </div>
+  </div>
+</div>
   <div class="relative rounded-2xl bg-white px-6 pt-10 pb-8   sm:mx-auto sm:max-w-lg sm:px-10">
-    <div class="mx-auto max-w-md">
+    <div class="mx-auto max-w-md ">
 
 
       <form action="" class="relative mx-auto w-max">
@@ -90,13 +117,13 @@ const Badges = () => {
     </div>
   </div>
 
-      <div className="container flex flex-wrap justify-evenly mx-auto my-9">
+      <div className="container flex flex-wrap justify-evenly mx-auto my-9 text-black">
         {filteredBadges.map(badge => (
           <div key={badge.id} className="border border-gray-300 rounded-lg p-4 w-48 text-center shadow-md flex flex-col justify-between"
            >
             
             <img src={badge.image} alt={badge.name} className="badge-image" />
-            <h3>{badge.name}</h3>
+            <h3 className='font-semibold'>{badge.name}</h3>
             <p>{badge.description}</p>
             <button className="bg-blue text-white"onClick={() => handlePrintBadge(badge.id)}>Print!</button>
           </div>
--- src/pages/users/Parent.jsx
+++ src/pages/users/Parent.jsx
@@ -11,16 +11,7 @@ const ParentDashboard = () => {
   return (
     <>
       {user ? (
-        // <div className="bg-logoYellow bg-opacity-50 max-w-80 rounded-xl flex flex-col justify-between min-h-96">
-        //   <h2 className='text-3xl mx-auto py-6'>User Information</h2>
-        //   <div className='flex flex-col justify-evenly min-h-72 mx-auto text-xl'>
-        //   <p><strong>Email:</strong> {user.email}</p>
-        //   <p><strong>User Type:</strong> {user.userType}</p>
-        //   <p><strong>Forename:</strong> {user.forename}</p>
-        //   <p><strong>Surname:</strong> {user.surname}</p>
-        //   {/* Add other fields as needed */}
-        // </div>
-        // </div>
+      
 <AvailabilityCalendar/>
 ) : (
   <p>Loading user information...</p>
